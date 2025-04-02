d29c9dbac9ed8866cb7c74a55fff314deb0873e5
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-04-03 02:28:21 +0900

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ActionButton } from '@/design-system/ui/action-button'
+
+function App() {
+  return (
+    <>
+      <h1>SEED + React + TS</h1>
+      <ActionButton>Click me</ActionButton>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import '@seed-design/css/all.css'
+import './index.css'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
